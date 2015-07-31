3a012bbb57ae66d7671204f25d1851b90e7d932e
Derek-Lun <dereklun@gmail.com>
2015-07-31 05:53:18 -0700

    added brush size function

--- mainV2.py
+++ mainV2.py
@@ -3,10 +3,31 @@ import copy
 import sys
 import collections
 import logWrite
+import constant
 
 frame_metadata = {}
 ROIs = set()
 is_mouse_down = False
+k = -1
+
+def num_key_lookup(num_key):
+    return { '1' : 1,
+        '2' : 2,
+        '3' : 3,
+        '4' : 4,
+        '5' : 5,
+        '6' : 6,
+        '7' : 7,
+        '8' : 8,
+        '9' : 9,
+    }.get(num_key, 0)
+
+def select_pixels(x, y):
+    global k
+    n = num_key_lookup(k)
+    for x_ in xrange(x-n, x+n+1):
+        for y_ in xrange(y-n, y+n+1):
+            ROIs.add((x_, y_))
 
 def on_mouse(event, x, y, flags, frame):
     frameRect = copy.copy(frame)
@@ -15,10 +36,10 @@ def on_mouse(event, x, y, flags, frame):
 
     if event == cv2.cv.CV_EVENT_LBUTTONDOWN:
         is_mouse_down = True
-        ROIs.add((x/4, y/4))
+        select_pixels(x/constant.RESCALE_FACTOR, y/constant.RESCALE_FACTOR)
         for ROI in ROIs:
             cv2.rectangle(frameRect, ROI, ROI, (0,0,255), 1)
-        frameRect = cv2.resize(frameRect, (0,0), fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
+        frameRect = cv2.resize(frameRect, (0,0), fx=constant.RESCALE_FACTOR, fy=constant.RESCALE_FACTOR, interpolation=cv2.INTER_NEAREST)
         cv2.imshow('frame',frameRect)
 
     elif event == cv2.cv.CV_EVENT_LBUTTONUP:
@@ -26,19 +47,20 @@ def on_mouse(event, x, y, flags, frame):
 
     elif event == cv2.cv.CV_EVENT_MOUSEMOVE:
         if (is_mouse_down == True):
-            ROIs.add((x/4, y/4))
+            select_pixels(x/constant.RESCALE_FACTOR, y/constant.RESCALE_FACTOR)
             for ROI in ROIs:
                 cv2.rectangle(frameRect, ROI, ROI, (0,0,255), 1)
-            frameRect = cv2.resize(frameRect, (0,0), fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
+            frameRect = cv2.resize(frameRect, (0,0), fx=constant.RESCALE_FACTOR, fy=constant.RESCALE_FACTOR, interpolation=cv2.INTER_NEAREST)
             cv2.imshow('frame',frameRect)
 
     elif event == cv2.cv.CV_EVENT_RBUTTONDOWN:
         #print 'End Right Mouse Position: '+str(x)+', '+str(y)
         ROIs.clear()
-        frameRect = cv2.resize(frameRect, (0,0), fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
+        frameRect = cv2.resize(frameRect, (0,0), fx=constant.RESCALE_FACTOR, fy=constant.RESCALE_FACTOR, interpolation=cv2.INTER_NEAREST)
         cv2.imshow('frame',frameRect)
 
 def main(argv):
+    global k
     if len(sys.argv) != 2:
         print "Usage: main.py <file name>"
         sys.exit()
@@ -68,30 +90,32 @@ def main(argv):
         cv2.namedWindow('frame')
         cv2.cv.SetMouseCallback('frame', on_mouse, frame)
 
-        frame = cv2.resize(frame, (0,0), fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
+        frame = cv2.resize(frame, (0,0), fx=constant.RESCALE_FACTOR, fy=constant.RESCALE_FACTOR, interpolation=cv2.INTER_NEAREST)
 
         cv2.imshow('frame',frame)
 
-        k=-1
-        while k not in (27, ord('u'), ord('n'), ord('s'), 2490368, 2621440, 2555904):
-            k = cv2.waitKey(0)
-            print str(k) + " was pressed"
+        k = 0
+        while k not in (chr(27), 'u', 'n', 's', 'w', 's', 'a', 'd'):
+            k = chr(cv2.waitKey(0) & 255)
+            print k + " was pressed"
 
-        if k==27:    # Esc key to stop
+        if k==chr(27):    # Esc key to stop
             break
-        elif k==ord('u'):
+        elif k=='u':
             ROIs.clear()
-            frame_metadata[int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES))] = "undefined"
-            print "undefined"
-        elif k==ord('n'):
+            frame_metadata[int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES))] = "UNCERTAIN"
+            print "UNCERTAIN"
+        elif k=='n':
             ROIs.clear()
-            frame_metadata[int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES))] = "nothing interesting"
-            print "nothing interesting"
-        elif k==2490368: # up arrow for fast reverse
-            cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)) - 25)
-        elif k==2621440: # down arrow for fast forward
-            cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)) + 23)
-        elif k==2555904: # right arrow for next frame
+            frame_metadata[int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES))] = "EMPTY"
+            print "EMPTY"
+        elif k=='w':
+            cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)) - constant.FRAME_SKIP - 1)
+        elif k=='s':
+            cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)) + constant.FRAME_SKIP - 1)
+        elif k=='a':
+            cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)) - 2)
+        elif k=='d':
             pass
 
         if len(ROIs) > 0:
