77fdef4866777bce6bfa2797f2b4293ac1247e88
Derek-Lun <dereklun@gmail.com>
2015-08-01 02:31:28 -0700

    added transparency to highlighted area

--- mainV2.py
+++ mainV2.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import copy
 import sys
 import collections
@@ -9,6 +10,7 @@ frame_metadata = {}
 ROIs = set()
 is_mouse_down = False
 k = -1
+highlighted_area = None
 
 def num_key_lookup(num_key):
     return { '1' : 1,
@@ -24,21 +26,24 @@ def num_key_lookup(num_key):
 
 def select_pixels(x, y):
     global k
+    global highlighted_area
+
     n = num_key_lookup(k)
     for x_ in xrange(x-n, x+n+1):
         for y_ in xrange(y-n, y+n+1):
             ROIs.add((x_, y_))
+            cv2.rectangle(highlighted_area, (x_, y_), (x_, y_), (0,0,255), 1)
 
 def on_mouse(event, x, y, flags, frame):
-    frameRect = copy.copy(frame)
-    global start_position
     global is_mouse_down
+    global highlighted_area
 
+    frameRect = copy.copy(frame)
+    
     if event == cv2.cv.CV_EVENT_LBUTTONDOWN:
         is_mouse_down = True
         select_pixels(x/constant.RESCALE_FACTOR, y/constant.RESCALE_FACTOR)
-        for ROI in ROIs:
-            cv2.rectangle(frameRect, ROI, ROI, (0,0,255), 1)
+        frameRect = cv2.addWeighted(frameRect, 0.7, highlighted_area, 0.3, 0)
         frameRect = cv2.resize(frameRect, (0,0), fx=constant.RESCALE_FACTOR, fy=constant.RESCALE_FACTOR, interpolation=cv2.INTER_NEAREST)
         cv2.imshow('frame',frameRect)
 
@@ -48,19 +53,22 @@ def on_mouse(event, x, y, flags, frame):
     elif event == cv2.cv.CV_EVENT_MOUSEMOVE:
         if (is_mouse_down == True):
             select_pixels(x/constant.RESCALE_FACTOR, y/constant.RESCALE_FACTOR)
-            for ROI in ROIs:
-                cv2.rectangle(frameRect, ROI, ROI, (0,0,255), 1)
+            frameRect = cv2.addWeighted(frameRect, 0.7, highlighted_area, 0.3, 0)
             frameRect = cv2.resize(frameRect, (0,0), fx=constant.RESCALE_FACTOR, fy=constant.RESCALE_FACTOR, interpolation=cv2.INTER_NEAREST)
             cv2.imshow('frame',frameRect)
 
     elif event == cv2.cv.CV_EVENT_RBUTTONDOWN:
-        #print 'End Right Mouse Position: '+str(x)+', '+str(y)
         ROIs.clear()
+        height, width, channels = frame.shape
+        highlighted_area = np.zeros((height, width, 3), np.uint8)
+
         frameRect = cv2.resize(frameRect, (0,0), fx=constant.RESCALE_FACTOR, fy=constant.RESCALE_FACTOR, interpolation=cv2.INTER_NEAREST)
         cv2.imshow('frame',frameRect)
 
 def main(argv):
     global k
+    global highlighted_area
+
     if len(sys.argv) != 2:
         print "Usage: main.py <file name>"
         sys.exit()
@@ -80,11 +88,14 @@ def main(argv):
         if int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)) == cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):
             break
 
+        height, width, channels = frame.shape
+        highlighted_area = np.zeros((height, width, 3), np.uint8)
+
         cv2.putText(frame,
         str(int(cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES))),
-        (25, 50),
+        (12, 15),
         cv2.FONT_HERSHEY_SIMPLEX,
-        2,
+        0.8,
         (255, 0, 0))
 
         cv2.namedWindow('frame')
